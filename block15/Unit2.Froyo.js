f60d6f598648474635717d4c2b0d7a15e5d1b5f6
Huckleberry11B <huckleberry@HuckleBerry11B>
2023-11-06 17:02:18 -0600

    final version of code

--- block15/Unit2.Froyo.js
+++ block15/Unit2.Froyo.js
@@ -1,15 +1,22 @@
-const userInputString = prompt(
-    "Please enter some Froyo flavors separated by commas.",
-    "vanilla,chocolate chip mint,vanilla,strawberry,coffee,coffee"
-  );
+const userInputString = prompt("Please enter some Froyo flavors separated by commas:");
 
-  const stringArray = userInputString.split(",");
+const stringArray = userInputString.trim().split(",");
 
-  const flavors = [];
+console.log(stringArray);
 
-for (let i = 0; i < stringArray.length; i++) {
-  const str = stringArray[i];
-  const flavors = count(str);
-  flavors.push(flavors);
-}
 
+// let elementCnt = stringArray.reduce((accVal, val) => {
+//   accVal[val] = (accVal[val] || 0) + 1;
+//   return accVal;
+//  }, {});
+const flavorCount = {}
+ for (let i = 0; i < stringArray.length; i++) {
+  const flavor = stringArray[i].trim()
+  if (!flavorCount[flavor]) {
+    flavorCount[flavor] = 0
+  }
+  flavorCount[flavor] += 1
+  console.log(flavor);
+ }
+ 
+ console.table(flavorCount)
